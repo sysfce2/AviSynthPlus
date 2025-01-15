0d3a484322cf4f7a26f6d628e99bdbaf220e34c8
Ferenc Pintér <pinterfer@gmail.com>
2025-01-15 15:17:33 +0100

    rstdoc: bump a year to 2025

--- distrib/docs/english/source/conf.py
+++ distrib/docs/english/source/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'AviSynth+'
-copyright = '2000-2021, AviSynth and AviSynth+ contributors'
+copyright = '2000-2025, AviSynth and AviSynth+ contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
