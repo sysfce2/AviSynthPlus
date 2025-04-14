sysfce2/AviSynthPlus

commit 61d2c9aa3ae41fc5def86cc30ff7f7c577fdcd16

Fix MacOS build, use common fill_chroma

avs_core/filters/planeswap.cpp
@@ -277,16 +277,6 @@ SwapUVToY::SwapUVToY(PClip _child, int _mode, IScriptEnvironment* env)
   }
 }
 
-template <typename T>
-static void fill_plane(BYTE* dstp, int rowsize, int height, int pitch, T val)
-{
-  rowsize /= sizeof(T);
-  for (int y = 0; y < height; ++y) {
-    std::fill_n(reinterpret_cast<T*>(dstp), rowsize, val);
-    dstp += pitch;
-  }
-}
-
 PVideoFrame __stdcall SwapUVToY::GetFrame(int n, IScriptEnvironment* env)
 {
   PVideoFrame src = child->GetFrame(n, env);
@@ -385,18 +375,15 @@ PVideoFrame __stdcall SwapUVToY::GetFrame(int n, IScriptEnvironment* env)
   BYTE* dstp_v = dst->GetWritePtr(PLANAR_V);
 
   if (vi.ComponentSize() == 1) {  // 8bit
-    fill_plane<BYTE>(dstp_u, rowsize, height, pitch, 0x80);
-    fill_plane<BYTE>(dstp_v, rowsize, height, pitch, 0x80);
+    fill_chroma<BYTE>(dstp_u, dstp_v, height, rowsize, pitch, 0x80);
   }
   else if (vi.ComponentSize() == 2) {  // 16bit
     uint16_t grey_val = 1 << (vi.BitsPerComponent() - 1); // 0x8000 for 16 bit
-    fill_plane<uint16_t>(dstp_u, rowsize, height, pitch, grey_val);
-    fill_plane<uint16_t>(dstp_v, rowsize, height, pitch, grey_val);
+    fill_chroma<uint16_t>(dstp_u, dstp_v, height, rowsize, pitch, grey_val);
   }
   else {  // 32bit(float)
     float grey_val = uv8tof(128);
-    fill_plane<float>(dstp_u, rowsize, height, pitch, grey_val);
-    fill_plane<float>(dstp_v, rowsize, height, pitch, grey_val);
+    fill_chroma<float>(dstp_u, dstp_v, height, rowsize, pitch, grey_val);
   }
 
   return dst;

commit 718a1380c0f416cb014a1575627b3f1d2601e848

Fix #434: YtoUV crash, regression since 61d2c9aa3ae41fc5def86cc30ff7f7c577fdcd16

avs_core/filters/planeswap.cpp
@@ -582,13 +582,13 @@ PVideoFrame __stdcall SwapYToUV::GetFrame(int n, IScriptEnvironment* env) {
   int pitch = dst->GetPitch(PLANAR_Y);
 
   if (vi.ComponentSize() == 1)  // 8bit
-    fill_plane<BYTE>(dstp, rowsize, vi.height, pitch, 0x7e);
+    fill_plane<BYTE>(dstp, vi.height, rowsize, pitch, 0x7e);
   else if (vi.ComponentSize() == 2) { // 16bit
     uint16_t luma_val = 0x7e << (vi.BitsPerComponent() - 8);
-    fill_plane<uint16_t>(dstp, rowsize, vi.height, pitch, luma_val);
+    fill_plane<uint16_t>(dstp, vi.height, rowsize, pitch, luma_val);
   }
   else { // 32bit(float)
-    fill_plane<float>(dstp, rowsize, vi.height, pitch, 126.0f / 256);
+    fill_plane<float>(dstp, vi.height, rowsize, pitch, 126.0f / 256);
   }
 
   return dst;